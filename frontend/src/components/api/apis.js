4b88844c7bed8be9e4fb328f8b352de1e6bf6ba3
Abdus Salam <abdus786salam@gmail.com>
2023-02-05 19:09:14 +0530

    redirects file added to deploy on netlify

---
+++ frontend/src/components/api/apis.js
@@ -0,0 +1,2 @@
+
+export const base_url=`https://tense-lime-outfit.cyclic.app`
--- frontend/src/redux/user/user.action.js
+++ frontend/src/redux/user/user.action.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
+import { base_url } from '../../components/api/apis'
 import * as types from './user.actionTypes'
 const postUserData=(data)=>(dispatch)=>{
     dispatch({type:types.USER_SIGNUP_REQUEST})
-    return axios.post(`http://localhost:5604/user/register`,data).then(res=>{
+    return axios.post(`${base_url}/user/register`,data).then(res=>{
        alert(res.data.msg,res.data.status)
         dispatch({type:types.USER_SIGNUP_SUCCESS})
     })
@@ -12,7 +13,7 @@ const postUserData=(data)=>(dispatch)=>{
 }
 const loginUser=(data)=>(dispatch)=>{
     dispatch({type:types.USER_LOGIN_REQUEST})
-    return axios.post(`http://localhost:5604/user/login`,data).then(res=>{
+    return axios.post(`${base_url}/user/login`,data).then(res=>{
         dispatch({type:types.USER_LOGIN_SUCCESS,payload:res.data})
         return res
     })
@@ -25,7 +26,7 @@ const getUser=()=>(dispatch)=>{
    const token=localStorage.getItem("token")
     dispatch({type:types.GET_USER_DETAILS_REQUEST})
     if(token){
-        return axios.get(`http://localhost:5604/user/details`,{
+        return axios.get(`${base_url}/user/details`,{
             headers: {
                 "Authorization": token,
             }
